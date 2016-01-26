d330030867193a486af309ddc124820232a5bc4e
Yoni <ymekuria@gmail.com>
2016-01-25 20:57:09 -0800

    added a basic server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+// var session = require('express-session');
+var morgan = require('morgan');
+
+var app = express();
+
+mongoose.connect();
+
+// Middleware. Add below as needed
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client'));// this serves all the static assests in the /client folder
+//app.use(express.cookieParser('shhhh, very secret'));// used for Auth uncomment when ready
+// app.use(session({secret: 'somesecret'})); // used for Auth
+
+
+
+var port = process.env.PORT || 5000;
+
+app.listen(port);
+
+module.exports = app;
