294f964675eb5c2bb399a239620efcf73c73585e
Erik Brown <erikdbrown@gmail.com>
2016-01-26 20:52:47 -0800

    new users are written to the users table and passwords are hashed

--- server/utils/routes.js
+++ server/utils/routes.js
@@ -1,23 +1,25 @@
 // add links to controllers here
 var db = require('../controllers/dbcontrollers.js');
+var userController = require('../controllers/userController.js')
 
 
 module.exports = function(app, express) {
 
  // add auth routes here	
-  app.get('/api/users'/***/);
-  app.post('/api/users'/***/);
+  // app.get('/api/users'/***/);
+  app.post('/api/users', userController.createLocalUser);
+  app.post('/api/users/login', userController.loginLocalUser);
  
   // these are the endpoints each user will have access to
-  app.post('/api/schema');
+  // app.post('/api/schema');
 
-  app.get('/api/schema'/***/);
+  // app.get('/api/schema'/***/);
 
 
-  app.post('/api/:username');
-  app.get('/api/:username'/***/);
-  app.put('/api/:username'/***/);
-  app.delete('/api/:username'/***/);
+  // app.post('/api/:username');
+  // app.get('/api/:username'/***/);
+  // app.put('/api/:username'/***/);
+  // app.delete('/api/:username'/***/);
 
 }
 
