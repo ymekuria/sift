fc8f03ccb4601b918d581d4f583ae8c19a9affb6
Yoni <ymekuria@gmail.com>
2016-01-29 16:40:27 -0800

    added specs testing for dynamic table creation

--- server/spec/dbcontroller-spec.js
+++ server/spec/dbcontroller-spec.js
@@ -27,7 +27,6 @@ describe("Posting User Schemas", function() {
     'lastname VARCHAR(120), ' +
     'streetaddress VARCHAR(200))', function(err, result) {
       if (err) { throw new Error(err); }
-    
     }); 
 
   
@@ -79,90 +78,90 @@ describe("Posting User Schemas", function() {
         var rows = JSON.parse(results.body);
         // expect(rows.length).to.equal(1);
         expect(rows[0].tablename).to.equal('yonim_test');
-        console.log('results in retrieve schema ', rows);
+        // console.log('results in retrieve schema ', rows);
       done();
       });    
     });
 
   it("Should delete a usersTable", function(done) {
-  request({ method: "GET",
+  request({ method: "DELETE",
             uri: "http://127.0.0.1:5001/api/deleteTable",
             json: { tableName: "yonim_test" }
-            
   }, function(err, results) {
     if (err) {throw new Error(err);}
-      
+      client.query('CREATE TABLE IF NOT EXISTS yonim_test ('+
+    'firstname VARCHAR(120), ' +
+    'lastname VARCHAR(120), ' +
+    'streetaddress VARCHAR(200))', function(err, result) {
+      if (err) { throw new Error(err); }
+    }); 
+      expect(results.body).to.equal("yonim_test");
+    });  
     done();
-    });    
+
   });
 
   it("Should delete a row from a usersTable", function(done) {
-  request({ method: "GET",
+  request({ method: "DELETE",
             uri: "http://127.0.0.1:5001/api/deleteRow",
-            json: { tableName: "yonim_test", columnName: "firstname", "value": "yoni"  }
-            
+            json: { tableName: "yonim_test", columnName: "firstname", value: "Yoni"  }
   }, function(err, results) {
     if (err) {throw new Error(err);}
-      
+      expect(results.body).to.equal("succesfully deleted Yoni");
     done();
     });    
   });
 
-    it("Should update a value in a usersTable", function(done) {
-  request({ method: "GET",
+  it("Should update a value in a usersTable", function(done) {
+  request({ method: "PUT",
             uri: "http://127.0.0.1:5001/api/updateValue",
-            json: { tableName: "yonim_test", columnName: "firstname", "newValue": "john", "oldValue": "yoni" }
-            
-  }, function(err, results) {
-    if (err) {throw new Error(err);}
-      
-    done();
-    });    
+            json: { "tableName": "yonim_test", "columnName": "firstname", "newValue": "john", "oldValue": "Yoni" }
+         }, function(err, results, body) {
+          if (err) {console.log(err);}
+          expect(results.body).to.equal("succesfully modified Yoni to john in yonim_test");
+          done();
+        });
+
+  });
+
+});  
+////////////////////////////////////////////////////////////////////////////////////////
+//////////////////GENERATING DYNAMIC TABLES BELOW //////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+describe("Generating dynamic tables", function() {
+  var client;
+
+  beforeEach(function(done) {
+    client = new pg.Client(db.connectionString);
+    client.connect();
+
+///POST REQUEST HERE 
+      done();
+
   });
  
-});  
+
+  afterEach(function() {
+    client.end();
+    });
 
 
-//   it("Should write to the database", function(done) {
-//      data = {
-//       firstname: 'Yoni',
-//       lastname: 'Mekuria',
-//       streetaddress: '5234 Callback Way'
-//     }
-//     client.query('INSERT INTO yonim_test (firstname, lastname, streetaddress) VALUES ($1, $2, $3)', [data.firstname, data.lastname, data.streetaddress], function(err, results) {
-//       if (err) { throw new Error(err); }
-//       client.query('SELECT * FROM yonim_test', function(err, results) {
-//         expect(results.rows.length).to.equal(1);
-//         done();
-//       })
-//     })
-  
-// });
-
-  // it("Should generate data and add fields to the database", function(done) {
-  //       client = new pg.Client(db.connectionString);
-  //   client.connect();
-    
-  //   request({ method: "POST",
-  //             uri: "http://127.0.0.1:5001/api/generateTable:?usr=yonim",
-  //             json: {"tableName": "test", "Name.firstName": "yes", "Name.lastName": "yes", "Address.streetAddress": "yes"}
-  //   }, function(error, firstResult) {
-  //     var queryString = "SELECT * FROM yonim_test";
-  //     //console.log('first result', firstResult);     
-  //     client.query(queryString, function(err, results) {
-  //       if(err) { throw err;}
-  //       //expect(results.rows.length).to.equal(10);
-  //       //expect(results.rows[0].username).to.equal("erikdbrown@gmail.com");
-  //       console.log('results in generate data', results.rows);
-
-  //     done();
-  //     });
-  //   });
-  // });
+  it("Should generate data and add fields to the database", function(done) {
+    request({ method: "POST",
+              uri: "http://127.0.0.1:5001/api/generateTable:?usr=yonim",
+              json: {"tableName": "test", "Name.firstName": "yes", "Name.lastName": "yes", "Address.streetAddress": "yes"}
+    }, function(error, firstResult) {
+      client.query("SELECT * FROM yonim_test", function(err, results) {
+        if(err) { throw err;}
+        expect(results.rows.length).to.equal(10);
+        // console.log('results in generate data', results.rows.length);
+        client.query("DROP TABLE IF EXISTS yonim_test;",function(err, data){
+          if (err){console.log(err)}
+        })
+      done();
+      });
+    });
+  });
 
 
-
-
-
-
-
+});
