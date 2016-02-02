087d40de47c755f0dbc2fc5eeba322f002a33267
ZACK DEAN <vanleuvenze@gmail.com>
2016-02-02 15:14:23 -0800

    general css formatting / react-redux-router dependency added

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+
 import routes from './config/routes.js'
 import { Router, browserHistory} from 'react-router'
+
+/*Redux Dependencies*/
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+
 require('./css/style.css')
 
 
