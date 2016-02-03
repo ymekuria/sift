2e628f2bef5086e63ada56fcf7c02ed6bdc85a40
Yoni <ymekuria@gmail.com>
2016-02-03 12:07:56 -0800

    created mockup and react skeleton for the user dashboard view

--- client/app.jsx
+++ client/app.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom'
 /*Routing*/
 import routes from './config/routes.js'
 import { Router, browserHistory} from 'react-router'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 /*Redux*/
 import { Provider } from 'react-redux'
 import store from './store.jsx'
+
 require('./css/style.css')
 
 //this is for use with the redux dev tools
@@ -16,8 +18,8 @@ require('./css/style.css')
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory}>{routes}</Router>
-  </Provider>,
-	document.getElementById('root')
-);
+  </Provider>, document.getElementById('root')
+ )
+
 
 
--- client/components/homepage.jsx
+++ client/components/homepage.jsx
@@ -1,5 +1,10 @@
-  import React, { Component } from 'react'
+import React, { Component } from 'react'
+import _ from 'lodash';
 
+// Material UI components
+import Paper from 'material-ui/lib/paper';
+import RaisedButton from 'material-ui/lib/raised-button';
+import FontIcon from 'material-ui/lib/font-icon';
 
 
 class Homepage extends Component {
@@ -16,23 +21,127 @@ class Homepage extends Component {
     }
   }
 
+  componentDidMount() {
+    //sync state with dataBase
+      // context: this,
+      //state: 'userTables'
+  
+  }
+
+  renderDashTable() {
+
+  }
 
   render() {
+    var testArr=['yoni', 'jon'];
     return(
-      <div className='homepage'>
-        <div className='banner'>
-          WELCOME TO SIFT
-          <br/>
-          INSERT COOL BANNER HERE
-          RENDER USER TABLES
+      <div className='container-fluid'>
+        <DashBanner/>
+
+        <div className='row'> 
+          <div className='col-md-12'>
+            <DashTable/>       
+          </div>    
         </div>
-        <div className='homeTableDisplay'>
-          DISPLAY USER TABLES HERE
-        </div>
-      </div>
+      </div>   
     )
   }
 }
+
+class DashTable extends Component {
+
+
+  render() {
+    const style = {
+    height: 200,
+    width: 200,
+    margin: 20,
+    textAlign: 'center',
+    display: 'inline-block',
+  };
+    return (
+      <div>
+        <Paper style={style}  zDepth={5} rounded={false}>
+            <h4></h4>
+            <RaisedButton label="Manage App" style={{margin: 5}} />
+
+       </Paper>
+     </div> 
+    ) 
+  }
+}
+
+
+class DashButtons extends Component {
+
+  render() {
+    return (
+      <div>
+        <RaisedButton label="QuickStart"  style={{margin:12}} />
+        <RaisedButton label="API Docs"  style={{margin:12}} />
+        <RaisedButton label="Examples" style={{margin:12}} />
+        <RaisedButton label="Fun"  style={{margin:12}} />
+      </div>
+    )
+  }
+}  
+
+class DashBanner extends Component {
+
+  render() {
+    return (
+    <div className='dashBanner'>
+      <div className='row'>
+          <h2 className="col-md-4 col-md-offset-4 " >
+            WELCOME TO SIFT
+          </h2>
+          <h4 className="col-md-4 col-md-offset-4 dashOneliner">
+            A Simple Integrated Frontend Toolkit
+          </h4>     
+      </div>
+    {/*row for buttons*/}
+      <div className='row'>
+        <div className='col-md-6 col-md-offset-3'>
+          <DashButtons/>
+        </div>
+      </div>
+    </div> 
+    )
+  }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
