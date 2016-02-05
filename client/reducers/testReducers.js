1a4145ed6f21d3e7c5e7a5d802d41a6f0f4bbaca
jfiamor <jonathanfiamor@gmail.com>
2016-02-04 20:56:59 -0800

    added some redux stuff

---
+++ client/reducers/testReducers.js
@@ -0,0 +1,74 @@
+//reducers
+import all from '../data/menu_options_all.js'
+import by_category from '../data/menu_options_by_category.js'
+import by_item from '../data/menu_options_by_item.js'
+import db_query_template from '../data/db_query_template.js'
+import Immutable from 'immutable'
+import { createTable } from '../utils/utils.js'
+
+const initialState = {
+  MenuOptions: {
+    currentCategory: 'names',
+    all: all.all,
+    byItem: by_item.items,
+    byCategory: by_category.categories
+  },
+  CurrentSelections: [],
+  BuildOrder: {}
+}
+
+const buildTable = (state = initialState, action) => {
+  switch(action.type) {
+
+    //======updating category to (re-renders option option list)======//
+    case 'update_category':
+      let stateRef = Immutable.fromJS(state);
+      let newState = stateRef.updateIn(['MenuOptions', 'currentCategory'], c => {
+        return action.newCategory;
+      })
+      return newState.toJS();
+
+    //======adding to build order (and to current selections)======//
+    case 'add_to_build':
+     return (Immutable.fromJS(state)
+      .updateIn(['BuildOrder'], sel => {
+
+        sel = sel.toJS();
+
+        if (!sel[action.category]) {
+          sel[action.category] = {};
+          sel[action.category][action.addition] = true;
+        } else {
+          sel[action.category][action.addition] = true;
+        }
+        return sel;
+      })
+      .updateIn(['CurrentSelections'], list => {
+        return list.push(action.addition)
+      }).toJS()
+    );
+
+    //======removing from currently selected======//
+    case 'remove_from_list':
+      return (
+        Immutable.fromJS(state)
+        .updateIn(['CurrentSelections'], list => {
+          return list.splice(action.id, 1)
+        }).toJS()
+      )
+
+    //======submitting table======//
+    case 'submit_table':
+      console.log('submitting table..?', state.BuildOrder)
+      createTable(action.tableName, state.BuildOrder);
+      //hook into backend!
+      //here we handle the post to our database
+      return state;
+
+    //=======default return state======//
+    default:
+      return state;
+  }
+
+}
+export default buildTable;
