db222e243844e0d9834cfc66b6bf60e9ca41e420
Erik Brown <erikdbrown@gmail.com>
2016-02-05 15:08:08 -0800

    changed how app is exported

--- devServer.js
+++ devServer.js
@@ -6,13 +6,13 @@ var config = require('./webpack.config.dev');
 var app = require('./server/server.js');
 var compiler = webpack(config);
 
-app.app.use(require('webpack-dev-middleware')(compiler, {
+app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
 
-app.app.use(require('webpack-hot-middleware')(compiler));
+app.use(require('webpack-hot-middleware')(compiler));
 
-app.app.get('*', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './client/index.html'));
 });
