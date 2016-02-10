4c2514cb993ef85ac7cf15aa87d76af06aaad48c
Erik Brown <erikdbrown@gmail.com>
2016-02-09 19:48:44 -0800

    fixed merge conflict

--- app/client/utils/utils.js
+++ app/client/utils/utils.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-
-
 //db call helpers
 //zen - 'https://api.github.com/zen'
 export const createTable = (selections) => {
@@ -55,58 +53,15 @@ export const getTables = (cb) => {
   })
   .then((response) => response.text())
   .then((text) => {
-<<<<<<< HEAD
     console.log('inside get tables', text);
     cb(JSON.parse(text))
   })
 }
 
-const postToTable = (username, tableName, row) => {
-  //api/postToTable:?usr=<username>
-  //add a row to a users table
-  let url = 'api/postToTable:?usr=' + username;
-  return fetch(url, {
-    credentials: 'same-origin',
-    method: 'POST',
-    headers: {},
-    body: {}
-  })
-  .then((response) => response.text())
-  .then((text) => {
-    console.log(text);
-  })
-}
-
-const updateValue = (username, tableName, row) => {
-  //api/updateValue:?usr=<username>
-  //update a specific value
-  let url = 'api/updateValue:?usr=' + username;
-  return fetch(url, {
-    credentials: 'same-origin',
-    method: 'PUT',
-    headers: {},
-    body: JSON.stringify({
-      tableName: tableName,
-      row: row
-    })
-  })
-  .then((response) => response.text())
-  .then((text) => {
-    console.log(text);
-=======
-    cb(JSON.parse(text))
->>>>>>> fixed api routes on front end so that it is based on paths vs. full URLs
-  })
-}
-
 export const deleteTable = (tableId, cb) => {
   //api/deleteTable:?usr=<username>
-<<<<<<< HEAD
   console.log('tableID in deletTabl ajax', tableId);
   let url = '/api/users/tables/' + tableId;
-=======
-  let url = 'api/users/tables/' + tableId;
->>>>>>> fixed api routes on front end so that it is based on paths vs. full URLs
   return fetch(url, {
     credentials: 'same-origin',
     method: 'DELETE',
