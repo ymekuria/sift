cd8a3e93cb4d315dd8855f40cacdc1b31753e7b5
Erik Brown <erikdbrown@gmail.com>
2016-02-11 15:37:51 -0800

    created cron job to delete rethink tables that have not been used in over seven days. updates active to false on postgres tables table

--- app/server/utils/dbconnect.js
+++ app/server/utils/dbconnect.js
@@ -35,6 +35,7 @@ client.query('CREATE TABLE IF NOT EXISTS Tables (' +
   'datatypes VARCHAR(200) DEFAULT null,' +
   'custom BOOLEAN DEFAULT false,' +
   'last_used timestamp DEFAULT current_date,' +
+  'active boolean DEFAULT true,' +
   'tablename VARCHAR(120) )', function(err, result) {
     if (err) { throw new Error(err); }
     console.log('"Tables" table created');
