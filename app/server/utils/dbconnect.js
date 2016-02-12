330bb00f8c1cea33623615ac2592afd307ce2dee
jfiamor <jonathanfiamor@gmail.com>
2016-02-11 16:11:49 -0800

    sizes stay consistant

--- app/server/utils/dbconnect.js
+++ app/server/utils/dbconnect.js
@@ -7,7 +7,7 @@ var connectionString;
 if (process.env.POSTGRES_PORT_5432_TCP_ADDR) {
   connectionString = 'postgres://postgres@postgres:5432';
 } else {
-  connectionString = 'postgres://postgres@localhost:5432/sift'
+  connectionString = 'postgres://seraph@localhost:5432/sift'
 }
 
 
