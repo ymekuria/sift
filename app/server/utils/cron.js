1961cd15f500adda59180ad3d351ceb04bbf4519
ZACK DEAN <vanleuvenze@gmail.com>
2016-02-17 17:00:56 -0800

    enabled cors

--- app/server/utils/cron.js
+++ app/server/utils/cron.js
@@ -1,32 +1,32 @@
-var client = require('../utils/dbconnect').client;
-var r = require('rethinkdb');
+// var client = require('../utils/dbconnect').client;
+// var r = require('rethinkdb');
 
-var CronJob = require('cron').CronJob;
-var job = new CronJob('00 00 00 * * 0-6', function() {
+// var CronJob = require('cron').CronJob;
+// var job = new CronJob('00 00 00 * * 0-6', function() {
 
-    if (process.env.RETHINK_PORT_8080_TCP_ADDR) {
-      rConnectConfig = { host: 'rethink', db: 'apiTables' }
-    } else {
-      rConnectConfig =  { host: 'localhost', db: 'apiTables' }
-    }
-    r.connect(rConnectConfig, function(err, conn) {
-      if (err) { console.log(err); }
-      connection = conn;
-    });
+//     if (process.env.RETHINK_PORT_8080_TCP_ADDR) {
+//       rConnectConfig = { host: 'rethink', db: 'apiTables' }
+//     } else {
+//       rConnectConfig =  { host: 'localhost', db: 'apiTables' }
+//     }
+//     r.connect(rConnectConfig, function(err, conn) {
+//       if (err) { console.log(err); }
+//       connection = conn;
+//     });
 
-    var queryString = "SELECT tablename FROM tables WHERE last_used > current_date - interval '8 days' AND active = true";
-    client.query(queryString, function(err, results) {
-      var tablenames = results.rows;
-      tablenames.forEach(function(tablename) {
-        r.table_drop(tablename);
-      });
-    });
-    var updateString = "UPDATE tables SET active = false WHERE last_used > current_date - interval '8 days' AND active = true";
-    client.query(updateString, function(err, results) {
-      console.log('Tables removed and updated.');
-    }) 
-  },
-  null,
-  false, /* Start the job right now */
-  'America/Los_Angeles'
-);
+//     var queryString = "SELECT tablename FROM tables WHERE last_used > current_date - interval '8 days' AND active = true";
+//     client.query(queryString, function(err, results) {
+//       var tablenames = results.rows;
+//       tablenames.forEach(function(tablename) {
+//         r.table_drop(tablename);
+//       });
+//     });
+//     var updateString = "UPDATE tables SET active = false WHERE last_used > current_date - interval '8 days' AND active = true";
+//     client.query(updateString, function(err, results) {
+//       console.log('Tables removed and updated.');
+//     }) 
+//   },
+//   null,
+//   false, /* Start the job right now */
+//   'America/Los_Angeles'
+// );
