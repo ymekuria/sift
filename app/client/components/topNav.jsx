878ee41b204374fd42055406a46e5a9dde42e002
Yoni <ymekuria@gmail.com>
2017-11-30 19:26:49 -0800

    prettier formatting

--- app/client/components/topNav.jsx
+++ app/client/components/topNav.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import { AppBar, Colors, FontIcon } from 'material-ui'
+import React, { Component } from 'react';
+import { AppBar, Colors, FontIcon } from 'material-ui';
 import IconMenu from 'material-ui/lib/menus/icon-menu';
 import IconButton from 'material-ui/lib/icon-button';
 import NavigationExpandMoreIcon from 'material-ui/lib/svg-icons/navigation/expand-more';
@@ -11,73 +11,77 @@ import ToolbarGroup from 'material-ui/lib/toolbar/toolbar-group';
 import ToolbarSeparator from 'material-ui/lib/toolbar/toolbar-separator';
 import ToolbarTitle from 'material-ui/lib/toolbar/toolbar-title';
 
-let styles = {
-  backgroundColor: '#333333',
-  width: '100vw',
-  position: 'relative',
-  bottom: '8px',
-  right: '10px',
-  marginLeft: '-5px'
-}
-let title = {
-  color: '#FDD53C',
-  position: 'relative',
-  marginTop: '5px',
-  right: '15px'
-}
-let separator = {
-  backgroundColor: '#FDD53C',
-  position: 'relative',
-  right: '18px',
-  marginTop: '3px'
-
-}
-let user = {
-  color: '#FDD53C',
-  position: 'relative',
-  top: '1px'
-}
-let signout = {
-  color: '#FDD53C',
-  marginTop: '14px',
-  textColor: '#333333'
-}
-let signOutGroup = {
-  paddingRight: '5px'
-}
 
 class TopNav extends Component {
-  constructor() {
-    super()
-  }
-
   signout() {
     localStorage.removeItem('sift-user');
     return fetch('/signout', {
       credentials: 'same-origin'
-    })
-    .then(() => window.location.assign('/'));
+    }).then(() => window.location.assign('/'));
   }
 
   render() {
+    const {toolbarStyle, signOutGroupStyle, signOutGroupStyle, singoutStyle, separatorStyle, titleStyle, userStyle } = styles;
     let username = JSON.parse(localStorage.getItem('sift-user'));
     return (
       <Toolbar color="#FDD53C" style={styles}>
         <ToolbarGroup>
-          <ToolbarTitle style={title} text='SIFT'/>
+          <ToolbarTitle style={title} text="SIFT" />
         </ToolbarGroup>
         <ToolbarGroup style={signOutGroup} float="right">
-          <ToolbarTitle style={user} text={ username.displayname}/>
-          <ToolbarSeparator className='separator' style={separator}/>
-          <RaisedButton labelColor='Black' backgroundColor='#FDD53C' style={signout} onClick={ this.signout } label={"Sign out"}/>
+          <ToolbarTitle style={user} text={username.displayname} />
+          <ToolbarSeparator className="separator" style={separator} />
+          <RaisedButton
+            labelColor="Black"
+            backgroundColor="#FDD53C"
+            style={signout}
+            onClick={this.signout}
+            label={'Sign out'}
+          />
         </ToolbarGroup>
       </Toolbar>
-    )
+    );
   }
 }
 
 TopNav.contextTypes = {
   router: React.PropTypes.object.isRequired
+};
+
+styles = {
+  toolbarStyle: {
+    backgroundColor: '#333333',
+    width: '100vw',
+    position: 'relative',
+    bottom: '8px',
+    right: '10px',
+    marginLeft: '-5px'
+  },
+  titleStyle: {
+    color: '#FDD53C',
+    position: 'relative',
+    marginTop: '5px',
+    right: '15px'
+  },
+  separatorStyle: {
+    backgroundColor: '#FDD53C',
+    position: 'relative',
+    right: '18px',
+    marginTop: '3px'
+  },
+  userStyle: {
+    color: '#FDD53C',
+    position: 'relative',
+    top: '1px'
+  },
+  signoutStyle: {
+    color: '#FDD53C',
+    marginTop: '14px',
+    textColor: '#333333'
+  },
+  signOutGroupStyle: {
+    paddingRight: '5px'
+  };
 }
 
 export default TopNav;
--- app/client/components/welcome.jsx
+++ app/client/components/welcome.jsx
@@ -1,27 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 //in here we should render links to our signin pages
 
 // export default Welcome
 
+//in here we should render links to our signin pages
+
+export const Welcome = () => {
+  console.log('in welcome');
+  return (
+    <div style={style}>
+      <a>Link to the sandbox</a>
+      <br />
+      <a>Link to signin</a>
+      <br />
+      <a>Link to signup</a>
+      <br />
+      this is our welcome page!
+    </div>
+  );
+};
+
 const style = {
   width: '500px',
   height: '500px',
   backgroundColor: 'green'
-}
-
-//in here we should render links to our signin pages
-
-export const Welcome = () => {
-  console.log('in welcome')
-  return (<div
-    style={style}>
-      <a>Link to the sandbox</a>
-      <br/>
-      <a>Link to signin</a>
-      <br/>
-      <a>Link to signup</a>
-      <br/>
-    this is our welcome page!
-    </div>)
-}
+};
