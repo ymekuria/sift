8b10664ca82d4a54177aed129760b67c612a856b
Yoni <ymekuria@gmail.com>
2017-12-06 16:25:54 -0800

    grouped data vis components together

--- app/client/components/DataEntry.jsx
+++ app/client/components/DataEntry.jsx
@@ -1,46 +1,51 @@
-
-import React, { Component } from 'react'
-import { createTable } from '../utils/utils.js'
-import Dropdown from './dropdown.jsx'
-import faker from 'faker'
-import h from '../config/helpers'
+import React, { Component } from 'react';
+import { createTable } from '../utils/utils.js';
+import Dropdown from './dropdown.jsx';
+import faker from 'faker';
+import h from '../config/helpers';
 /*
   == Material UI componenets ==
 */
-import Paper from 'material-ui/lib/paper'
-import { Menu, MenuItem } from 'material-ui'
+import Paper from 'material-ui/lib/paper';
+import { Menu, MenuItem } from 'material-ui';
 
 //refactor into css file
-require('../../node_modules/react-select/dist/react-select.css')
-
+require('../../node_modules/react-select/dist/react-select.css');
 
 class DataEntry extends Component {
-
   componentDidMount() {
     const { store } = this.context;
     this.unsubscribe = store.subscribe(() => {
-      this.forceUpdate()
-      }
-    );
+      this.forceUpdate();
+    });
   }
 
   componentWillUnmount() {
     this.unsubscribe();
   }
 
-  render () {
-    const { MenuOptions, CurrentSelections, loading } = this.context.store.getState().buildTable;
+  render() {
+    const {
+      MenuOptions,
+      CurrentSelections,
+      loading
+    } = this.context.store.getState().buildTable;
     return (
-      <div className='dataEntry'>
-        <Paper className='select'>
-          <Dropdown loading={loading} currentSelections={CurrentSelections} menuOptions={MenuOptions} className='dropdown'/>
+      <div className="dataEntry">
+        <Paper className="select">
+          <Dropdown
+            loading={loading}
+            currentSelections={CurrentSelections}
+            menuOptions={MenuOptions}
+            className="dropdown"
+          />
         </Paper>
       </div>
-    )
+    );
   }
 }
 DataEntry.contextTypes = {
   store: React.PropTypes.object
-}
+};
 
-export default DataEntry
+export default DataEntry;
--- app/client/components/datavis/Tree.jsx
+++ app/client/components/datavis/Tree.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
-import showGraph from './dndTree.js'
-import $ from 'jquery'
+import React, { Component } from 'react';
+import showGraph from './dndTree.js';
+import $ from 'jquery';
 
 class Tree extends Component {
-
   render() {
     $('#tree-container').empty();
     showGraph(this.props.data, this.props.removeNode);
-		return <div id='tree-container'></div>
-	}
+    return <div id="tree-container" />;
+  }
 }
 
 export default Tree;
